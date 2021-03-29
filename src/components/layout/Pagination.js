078df4163b72fdc681534426755385b7081de84b
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2021-03-29 10:54:29 -0700

    Changed the pagination button to button element instead of an anchor

--- src/components/layout/Pagination.js
+++ src/components/layout/Pagination.js
@@ -15,9 +15,9 @@ const Pagination = (props) => {
             className={currentPage === page ? "page-item active" : "page-item"}
             key={page}
           >
-            <a className="page-link" onClick={() => onPageChange(page)}>
+            <button className="page-link" onClick={() => onPageChange(page)}>
               {page}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
